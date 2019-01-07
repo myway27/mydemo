d7f3452e43521b4465325124a9122eb96cd77a6b
myway27 <807648658@qq.com>
2019-01-07 22:37:54 +0800

    pro init

---
+++ my-travel/mypro/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ my-travel/mypro/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/page/home/Home'
+import List from '@/page/list/List'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/list',
+    name: 'List',
+    component: List
+  }]
+})
